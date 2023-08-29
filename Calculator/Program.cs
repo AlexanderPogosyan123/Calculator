6b427fbcaba2265ea6f6a76940acc5f3bdf29eda
Alexander Pogosyan <aleksandrpogosyan76@gmail.com>
2023-08-29 16:40:58 +0400

    ввод чисел

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -5,6 +5,12 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
-        }
+            Console.WriteLine("enter num1");
+            int number1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter num2");
+            int number2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter operation");
+            char ch = Convert.ToChar(Console.ReadLine());
+            
     }
 }
